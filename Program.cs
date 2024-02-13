d01faec30cacfe86a0fc7ff0365668c2063377c4
Rafael Gioffi <rafaelgioffi@gmail.com>
2024-02-12 23:30:32 -0300

    Faltam testes mais apurados...

--- Program.cs
+++ Program.cs
@@ -56,6 +56,10 @@ namespace CleanDN_CC_v2
                     {
                         string[] allLines = File.ReadAllLines(file);
 
+                        string[] procDate = allLines[0].Split('#');
+                        string procDateFiltered = DateTime.Parse(procDate[1]).ToString("yyyyMMdd");
+                        string timeFile = File.GetCreationTime(file).ToString("HHmmss");
+
                         foreach (var l in allLines) //validação de cada linha...
                         {
                             string[] DnActual = l.Split('#');
@@ -78,12 +82,19 @@ namespace CleanDN_CC_v2
                             string actualName = actual.Last();
                             actual = actualName.Split('.');
                             actualName = $"{actual[0]}.{actual[1]}";
-                            //FileWithBanName = $"{FileToProcess}.D{DateTime.Now.ToString("yyyyMMdd")}.T{DateTime.Now.ToString("HHmmss")}.txt";   //nome do arquivo com erros...
-                            FileInvalidName = $"{actualName}.TXT.ERROR.D{DateTime.Now.ToString("yyyyMMdd")}.T{DateTime.Now.ToString("HHmmss")}";   //nome do arquivo com erros...
+                            
+                            //FileInvalidName = $"{actualName}.TXT.ERROR.D{DateTime.Now.ToString("yyyyMMdd")}.T{DateTime.Now.ToString("HHmmss")}";   //nome do arquivo com erros...
+                            FileInvalidName = $"{actualName}.TXT.ERROR.{procDateFiltered}.{timeFile}";   //nome do arquivo com erros...
 
                             //cria as pastas caso não existam...
                             if (!Directory.Exists(FolderToBackup)) { Directory.CreateDirectory(FolderToBackup); }
                             if (!Directory.Exists(FolderNonProcessed)) { Directory.CreateDirectory(FolderNonProcessed); }
+
+                            //Verifica se o arquivo já existe na pasta e caso exista, add 3s no timestamp...
+                            if (File.Exists(FolderNonProcessed + FileInvalidName)) {
+                                int tempTime = int.Parse(timeFile) + 3;
+                                FileValidName = $"{actualName}.TXT.ERROR.{procDateFiltered}.{tempTime}";
+                            }
 
                             using (StreamWriter sw = new StreamWriter(FolderNonProcessed + FileInvalidName)) //cria o arquivo somente dos banidos...
                             {
@@ -95,17 +106,32 @@ namespace CleanDN_CC_v2
                             Log($"Gerado o arquivo com os DNs inválidos em {FolderNonProcessed}{FileInvalidName}...", false);
                             Console.WriteLine($"Gerado o arquivo com os DNs inválidos em {FolderNonProcessed}{FileInvalidName}...");
 
-                            Thread.Sleep(3000); //intervalo de 3s por segurança...
+                            //Thread.Sleep(3000); //intervalo de 3s por segurança...
 
-                            string BkpOriginalFile = $"{actualName}.TXT.ORIGINAL.D{DateTime.Now.ToString("yyyyMMdd")}.T{DateTime.Now.ToString("HHmmss")}";
+                            //string BkpOriginalFile = $"{actualName}.TXT.ORIGINAL.D{DateTime.Now.ToString("yyyyMMdd")}.T{DateTime.Now.ToString("HHmmss")}";
+                            string BkpOriginalFile = $"{actualName}.TXT.ORIGINAL.{procDateFiltered}.{timeFile}";
+
+                            //Verifica se o arquivo já existe na pasta e caso exista, add 3s no timestamp...
+                            if (File.Exists(FolderToBackup + BkpOriginalFile))
+                            {
+                                int tempTime = int.Parse(timeFile) + 3;
+                                BkpOriginalFile = $"{actualName}.TXT.ORIGINAL.{procDateFiltered}.{tempTime}";
+                            }
+
                             File.Move(file, FolderToBackup + BkpOriginalFile);    //renomeia o arquivo original para não ser processado...
                             Log($"Arquivo {actualName}.TXT renomeado e movido para {FolderToBackup}{BkpOriginalFile}...", false);
                             Console.WriteLine($"Arquivo {actualName}.TXT renomeado e movido para {FolderToBackup}{BkpOriginalFile}...");
 
-                            Thread.Sleep(3000); //intervalo de 3s por segurança...
+                            //Thread.Sleep(3000); //intervalo de 3s por segurança...
 
                             //FileWithoutBanName = $"{actualName}.TXT.SB.D{DateTime.Now.ToString("yyyyMMdd")}.T{DateTime.Now.ToString("HHmmss")}";   //nome do arquivo sem ignorados...
                             FileValidName = $"{actualName}.TXT";   //nome do arquivo sem ignorados...
+
+                            while(File.Exists(Folder + FileValidName))
+                            {
+                                int tempTime = int.Parse(timeFile) + 3;
+                                FileValidName = $"{actualName}.TXT.{procDateFiltered}.{tempTime}";
+                            }
 
                             using (StreamWriter sw = new StreamWriter(Folder + FileValidName)) //cria o arquivo somente sem os banidos...
                             {
